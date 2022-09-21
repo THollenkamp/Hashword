21ffdbc57349f658436825d4ac0db49dccc31cb3
Tucker <crushedaf@gmail.com>
2022-09-20 20:04:34 -0500

    Updated formatting

--- run.bat
+++ run.bat
@@ -1,7 +1,3 @@
 @echo off
-echo NOTICE: This project depends on your specific hardware settings,
-echo so making any hardware changes to your computer may change any passwords
-echo that have been generated. Please make sure to make backups of any passwords
-echo before making any hardware changes to your computer!
 java -jar target/Hashword.jar
 pause
