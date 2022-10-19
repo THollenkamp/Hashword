50ead5ded2986b49607aaad99cb051828f420378
Tucker <crushedaf@gmail.com>
2022-10-19 01:25:52 -0500

    Updated Structure

--- src/main/java/me/tucker/hashword/hardware/MacInstance.java
+++ src/main/java/me/tucker/hashword/hardware/MacInstance.java
@@ -1,10 +1,12 @@
 package me.tucker.hashword.hardware;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class MacInstance extends OSInstance {
+
+    @Override
+    public String getCmd() {
+        return "system_profiler SPSoftwareDataType SPHardwareDataType";
+    }
+
     @Override
     public String getMonitorInfo() {
         return "null";
@@ -22,18 +24,11 @@ public class MacInstance extends OSInstance {
 
     @Override
     public String getHardwareInfo() {
-        StringBuilder str = new StringBuilder();
-        try {
-            Process proc = Runtime.getRuntime().exec("system_profiler SPSoftwareDataType SPHardwareDataType");
-            BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-            String output;
-            while ((output = stdInput.readLine()) != null) {
-                if (output.contains("Hardware UUID:") || output.contains("Serial Number"))
-                    str.append(output);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return str.toString();
+        return getDataPoint("Hardware UUID:") + getDataPoint("Serial Number");
+    }
+
+    @Override
+    public String getRamInfo() {
+        return null;
     }
 }
